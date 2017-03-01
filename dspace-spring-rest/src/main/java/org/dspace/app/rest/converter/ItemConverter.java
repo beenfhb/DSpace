c3e38c94988b73a9674e6f944b5f9a5ae2e04edf
Terry W Brady <twb27@georgetown.edu>
2017-02-28 16:15:38 -0800

    add bitstreams to item

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/converter/ItemConverter.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/converter/ItemConverter.java
@@ -7,7 +7,13 @@
  */
 package org.dspace.app.rest.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dspace.app.rest.model.BitstreamRest;
 import org.dspace.app.rest.model.ItemRest;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Bundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +28,7 @@ import org.springframework.stereotype.Component;
 public class ItemConverter extends DSpaceObjectConverter<org.dspace.content.Item, org.dspace.app.rest.model.ItemRest> {
 	@Autowired
 	private CollectionConverter collectionConverter;
+	private BitstreamConverter bitstreamConverter;
 
 	@Override
 	public ItemRest fromModel(org.dspace.content.Item obj) {
@@ -30,8 +37,24 @@ public class ItemConverter extends DSpaceObjectConverter<org.dspace.content.Item
 		item.setDiscoverable(obj.isDiscoverable());
 		item.setWithdrawn(obj.isWithdrawn());
 		item.setLastModified(obj.getLastModified());
-		//item.setTemplateItemOf(collectionConverter.fromModel(obj.getTemplateItemOf()));
-		//item.setOwningCollection(collectionConverter.fromModel(obj.getOwningCollection()));
+		try {
+			item.setTemplateItemOf(collectionConverter.fromModel(obj.getTemplateItemOf()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			item.setOwningCollection(collectionConverter.fromModel(obj.getOwningCollection()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		List<BitstreamRest> bitstreams = new ArrayList<BitstreamRest>();
+		for (Bundle bun : obj.getBundles()) {
+			for (Bitstream bit : bun.getBitstreams()) {
+				BitstreamRest bitrest = bitstreamConverter.fromModel(bit);
+				bitstreams.add(bitrest);
+			}
+		}
+		item.setBitstreams(bitstreams);
 		return item;
 	}
 
