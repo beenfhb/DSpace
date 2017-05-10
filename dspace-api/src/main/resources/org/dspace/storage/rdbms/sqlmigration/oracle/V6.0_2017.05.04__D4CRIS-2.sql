f8594a80832ef97ff19a87290a33fae5fd29ce94
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-05-10 19:02:34 +0200

    D4CRIS-2 DCRIS table migration

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2017.05.04__D4CRIS-2.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2017.05.04__D4CRIS-2.sql
@@ -16,3 +16,24 @@ ALTER TABLE cris_rpage RENAME COLUMN epersonid to epersonid_legacy_id;
 ALTER TABLE cris_rpage ADD COLUMN epersonid RAW(16);
 UPDATE cris_rpage SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_rpage.epersonid_legacy_id = eperson.eperson_id);
 ALTER TABLE cris_rpage DROP COLUMN epersonid_legacy_id;
+
+ALTER TABLE cris_relpref RENAME COLUMN itemid to itemid_legacy_id;
+ALTER TABLE cris_relpref ADD COLUMN itemid RAW(16);
+UPDATE cris_relpref SET itemid = (SELECT item.uuid FROM item WHERE cris_relpref.itemid_legacy_id = item.item_id);
+ALTER TABLE cris_relpref DROP COLUMN itemid_legacy_id;
+
+ALTER TABLE cris_relpref RENAME COLUMN itemid to itemid_legacy_id;
+ALTER TABLE cris_relpref ADD COLUMN itemid RAW(16);
+UPDATE cris_relpref SET itemid = (SELECT item.uuid FROM item WHERE cris_relpref.itemid_legacy_id = item.item_id);
+ALTER TABLE cris_relpref DROP COLUMN itemid_legacy_id;
+CREATE INDEX cris_relpref_itemid on cris_relpref(itemid);
+
+ALTER TABLE cris_subscription RENAME COLUMN epersonid to epersonid_legacy_id;
+ALTER TABLE cris_subscription ADD COLUMN epersonid RAW(16);
+UPDATE cris_subscription SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_subscription.epersonid_legacy_id = eperson.eperson_id);
+ALTER TABLE cris_subscription DROP COLUMN epersonid_legacy_id;
+
+ALTER TABLE cris_statsubscription RENAME COLUMN epersonid to epersonid_legacy_id;
+ALTER TABLE cris_statsubscription ADD COLUMN epersonid RAW(16);
+UPDATE cris_statsubscription SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_statsubscription.epersonid_legacy_id = eperson.eperson_id);
+ALTER TABLE cris_statsubscription DROP COLUMN epersonid_legacy_id;
--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V6.0_2017.05.04__D4CRIS-2.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V6.0_2017.05.04__D4CRIS-2.sql
@@ -17,3 +17,21 @@ ALTER TABLE cris_rpage ADD COLUMN epersonid UUID;
 UPDATE cris_rpage SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_rpage.epersonid_legacy_id = eperson.eperson_id);
 ALTER TABLE cris_rpage DROP COLUMN epersonid_legacy_id;
 CREATE INDEX cris_rpage_eperson on cris_rpage(epersonid);
+
+ALTER TABLE cris_relpref RENAME COLUMN itemid to itemid_legacy_id;
+ALTER TABLE cris_relpref ADD COLUMN itemid UUID;
+UPDATE cris_relpref SET itemid = (SELECT item.uuid FROM item WHERE cris_relpref.itemid_legacy_id = item.item_id);
+ALTER TABLE cris_relpref DROP COLUMN itemid_legacy_id;
+CREATE INDEX cris_relpref_itemid on cris_relpref(itemid);
+
+ALTER TABLE cris_subscription RENAME COLUMN epersonid to epersonid_legacy_id;
+ALTER TABLE cris_subscription ADD COLUMN epersonid UUID;
+UPDATE cris_subscription SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_subscription.epersonid_legacy_id = eperson.eperson_id);
+ALTER TABLE cris_subscription DROP COLUMN epersonid_legacy_id;
+CREATE INDEX cris_subscription_epersonid on cris_subscription(epersonid);
+
+ALTER TABLE cris_statsubscription RENAME COLUMN epersonid to epersonid_legacy_id;
+ALTER TABLE cris_statsubscription ADD COLUMN epersonid UUID;
+UPDATE cris_statsubscription SET epersonid = (SELECT eperson.uuid FROM eperson WHERE cris_statsubscription.epersonid_legacy_id = eperson.eperson_id);
+ALTER TABLE cris_statsubscription DROP COLUMN epersonid_legacy_id;
+CREATE INDEX cris_statsubscription_epersonid on cris_statsubscription(epersonid);
