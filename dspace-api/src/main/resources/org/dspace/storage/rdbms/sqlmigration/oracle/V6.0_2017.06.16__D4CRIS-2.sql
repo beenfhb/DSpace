2d8fcac94a7d054d7571a6ff99e965c663fae1c9
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-06-19 12:04:25 +0200

    D4CRIS-2 fix oracle sql script

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2017.06.16__D4CRIS-2.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/oracle/V6.0_2017.06.16__D4CRIS-2.sql
@@ -8,7 +8,12 @@
 
 --migrate cris_metrics
 ALTER TABLE cris_metrics RENAME COLUMN resourceid to resourceid_legacy_id;
-ALTER TABLE cris_metrics ADD resourceid UUID;
+ALTER TABLE cris_metrics ADD resourceid RAW(36);
+ALTER TABLE CRIS_RPAGE MODIFY (uuid VARCHAR2(36) );
+ALTER TABLE CRIS_PROJECT MODIFY (uuid VARCHAR2(36) );
+ALTER TABLE CRIS_ORGUNIT MODIFY (uuid VARCHAR2(36) );
+ALTER TABLE CRIS_DO MODIFY (uuid VARCHAR2(36) );
+
 UPDATE cris_metrics SET resourceid = (SELECT bitstream.uuid FROM bitstream WHERE cris_metrics.resourceid_legacy_id = bitstream.bitstream_id) WHERE cris_metrics.resourcetypeid = 0;
 UPDATE cris_metrics SET resourceid = (SELECT bundle.uuid FROM bundle WHERE cris_metrics.resourceid_legacy_id = bundle.bundle_id) WHERE cris_metrics.resourcetypeid = 1;
 UPDATE cris_metrics SET resourceid = (SELECT item.uuid FROM item WHERE cris_metrics.resourceid_legacy_id = item.item_id) WHERE cris_metrics.resourcetypeid = 2;
@@ -17,10 +22,10 @@ UPDATE cris_metrics SET resourceid = (SELECT community.uuid FROM community WHERE
 UPDATE cris_metrics SET resourceid = (SELECT site.uuid FROM site WHERE cris_metrics.resourceid_legacy_id = site.site_id) WHERE cris_metrics.resourcetypeid = 5;
 UPDATE cris_metrics SET resourceid = (SELECT epersongroup.uuid FROM epersongroup WHERE cris_metrics.resourceid_legacy_id = epersongroup.epersongroup_id) WHERE cris_metrics.resourcetypeid = 6;
 UPDATE cris_metrics SET resourceid = (SELECT eperson.uuid FROM eperson WHERE cris_metrics.resourceid_legacy_id = eperson.eperson_id) WHERE cris_metrics.resourcetypeid = 7;
-UPDATE cris_metrics SET resourceid = (SELECT cris_rpage.uuid FROM cris_rpage WHERE cris_metrics.resourceid_legacy_id = cris_rpage.id) WHERE cris_metrics.resourcetypeid = 9;
-UPDATE cris_metrics SET resourceid = (SELECT cris_project.uuid FROM cris_project WHERE cris_metrics.resourceid_legacy_id = cris_project.id) WHERE cris_metrics.resourcetypeid = 10;
-UPDATE cris_metrics SET resourceid = (SELECT cris_orgunit.uuid FROM cris_orgunit WHERE cris_metrics.resourceid_legacy_id = cris_orgunit.id) WHERE cris_metrics.resourcetypeid = 11;
-UPDATE cris_metrics SET resourceid = (SELECT cris_do.uuid FROM cris_do WHERE cris_metrics.resourceid_legacy_id = cris_do.id) WHERE cris_metrics.resourcetypeid > 11;
+UPDATE cris_metrics SET resourceid = utl_raw.cast_to_raw((SELECT cris_rpage.uuid FROM cris_rpage WHERE cris_metrics.resourceid_legacy_id = cris_rpage.id)) WHERE cris_metrics.resourcetypeid = 9;
+UPDATE cris_metrics SET resourceid = utl_raw.cast_to_raw((SELECT cris_project.uuid FROM cris_project WHERE cris_metrics.resourceid_legacy_id = cris_project.id)) WHERE cris_metrics.resourcetypeid = 10;
+UPDATE cris_metrics SET resourceid = utl_raw.cast_to_raw((SELECT cris_orgunit.uuid FROM cris_orgunit WHERE cris_metrics.resourceid_legacy_id = cris_orgunit.id)) WHERE cris_metrics.resourcetypeid = 11;
+UPDATE cris_metrics SET resourceid = utl_raw.cast_to_raw((SELECT cris_do.uuid FROM cris_do WHERE cris_metrics.resourceid_legacy_id = cris_do.id)) WHERE cris_metrics.resourcetypeid > 11;
 
 ALTER TABLE cris_metrics DROP COLUMN resourceid_legacy_id;
 CREATE INDEX cris_metrics_resourceid on doi2item(resourceid);
