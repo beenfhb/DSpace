72d34f75852a4f3289c34cb2f5cc45b4c58b597a
Luigi Andrea Pascarelli <luigiandrea.pascarelli@4science.it>
2017-12-04 20:28:53 +0100

    D4CRIS-369 add logic to manage apa, vancouver, chicago author's style

--- dspace-api/src/main/java/org/dspace/content/integration/crosswalks/VirtualFieldAuthors.java
+++ dspace-api/src/main/java/org/dspace/content/integration/crosswalks/VirtualFieldAuthors.java
@@ -16,12 +16,14 @@ import org.dspace.content.Item;
 import org.dspace.content.IMetadataValue;
 
 /**
- * Costruisce i singoli autori a partire dalla stringa allauthor
+ * Build author citation format for APA, VANCOUVER, CHICAGO
  * 
- * @author bollini
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ * @author Luigi Andrea Pascarelli (luigiandrea.pascarelli at 4science.it)
+ *
  */
-public class VirtualFieldBibtexAuthors implements VirtualFieldDisseminator, VirtualFieldIngester {
-	private static Logger log = Logger.getLogger(VirtualFieldBibtexAuthors.class);
+public class VirtualFieldAuthors implements VirtualFieldDisseminator, VirtualFieldIngester {
+	private static Logger log = Logger.getLogger(VirtualFieldAuthors.class);
 
 	public String[] getMetadata(Item item, Map<String, String> fieldCache, String fieldName) {
 
@@ -30,22 +32,115 @@ public class VirtualFieldBibtexAuthors implements VirtualFieldDisseminator, Virt
 		// Get the citation from the item
 		List<IMetadataValue> dcvs = item.getMetadataValueInDCFormat(metadata);
 
-		if (dcvs != null && dcvs.size() > 0) {
-			StringBuffer sb = new StringBuffer();
-			for (IMetadataValue a : dcvs) {
-				String[] split = a.getValue().split(", ");
-				int splitLength = split.length;
-				String str = (splitLength > 1) ? split[1] : "";
-				String str2 = split[0];
-				if (StringUtils.isNotBlank(str2)) {
-					sb.append(str).append(" ");
+		switch (fieldName) {
+		case "virtual.authors.apa":
+			return processAPAStyle(dcvs);
+		case "virtual.authors.vancouver":
+			return processVancouverStyle(dcvs);
+		case "virtual.authors.chicago":
+			return processChicagoStyle(dcvs);
+		default:
+			if (dcvs != null && dcvs.size() > 0) {
+				StringBuffer sb = new StringBuffer();
+				for (IMetadataValue a : dcvs) {
+					String[] split = a.getValue().split(", ");
+					int splitLength = split.length;
+					String str = (splitLength > 1) ? split[1] : "";
+					String str2 = split[0];
+					if (StringUtils.isNotBlank(str2)) {
+						sb.append(str).append(" ");
+					}
+					sb.append(str2).append(" and ");
 				}
-				sb.append(str2).append(" and ");
+				return new String[] { sb.substring(0, sb.length() - 5) };
 			}
-			return new String[] { sb.substring(0, sb.length() - 5) };
 		}
-
 		return null;
+	}
+
+	private String[] processChicagoStyle(List<IMetadataValue> dcvs) {
+		String[] results = new String[dcvs.size()];
+		int index = 0;
+		for(IMetadataValue imv : dcvs) {
+    		int separator = StringUtils.indexOf(imv.getValue(), ",");
+    		String lastname = StringUtils.substring(imv.getValue(), 0, separator);
+    		String firstname = StringUtils.substring(imv.getValue(), separator+1);
+    		
+    		String result = lastname;
+    		if(StringUtils.isNotBlank(firstname)) {
+    			if(index > 0) {
+    				result += " " + firstname.trim();	
+    			}
+    			else {
+    				result += ", " + firstname.trim();
+    			}
+    		}
+    		if(index < dcvs.size()-1) {
+    			result += ",";
+    		}
+    		
+    		if(index == dcvs.size()-2) {
+    			result += " and ";	
+    		}
+
+    		results[index] = result;
+    		index++;
+		}
+		return results;
+	}
+
+	private String[] processVancouverStyle(List<IMetadataValue> dcvs) {
+		String[] results = new String[dcvs.size()];
+		int index = 0;
+		for(IMetadataValue imv : dcvs) {
+    		int separator = StringUtils.indexOf(imv.getValue(), ",");
+    		String lastname = StringUtils.substring(imv.getValue(), 0, separator);
+    		String firstname = StringUtils.substring(imv.getValue(), separator+1);
+    		
+    		String result = lastname;
+    		if(StringUtils.isNotBlank(firstname)) {
+    			result += ", " + firstname.trim().substring(0, 1) + ".";
+    		}
+    		if(index < dcvs.size()-2) {
+    			result += ",";
+    		}
+    		
+    		if(index == dcvs.size()-2) {
+    			result += " and ";	
+    		}
+    		
+    		results[index] = result;
+    		index++;
+		}
+		return results;
+	}
+
+	private String[] processAPAStyle(List<IMetadataValue> dcvs) {
+		String[] results = new String[dcvs.size()];
+		int index = 0;
+		for(IMetadataValue imv : dcvs) {
+    		int separator = StringUtils.indexOf(imv.getValue(), ",");
+    		String lastname = StringUtils.substring(imv.getValue(), 0, separator);
+    		String firstname = StringUtils.substring(imv.getValue(), separator+1);
+    		
+    		String result = lastname;
+    		if(StringUtils.isNotBlank(firstname)) {
+    			String initialFirstname = firstname.trim().substring(0, 1)+".";
+    			String totalFirstname = firstname.trim();
+    			if(!initialFirstname.equals(totalFirstname)) {
+    				result += ", " + initialFirstname +" [" + totalFirstname + "]";
+    			}
+    			else {
+    				result += ", " + initialFirstname;
+    			}
+    		}
+    		if(index < dcvs.size()-1) {
+    			result += ",";
+    		}
+    		results[index] = result;
+    		index++;
+		}
+		return results;
 	}
 
 	public boolean addMetadata(Item item, Map<String, String> fieldCache, String fieldName, String value) {
